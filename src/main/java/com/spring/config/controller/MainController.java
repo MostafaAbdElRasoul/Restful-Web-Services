fa0a5346266ba1aca984ea364975992586a273e1
mostafa <mosta.abdo250@gmail.com>
2022-02-14 20:28:13 +0200

    handle exception in restful

--- src/main/java/com/spring/config/controller/MainController.java
+++ src/main/java/com/spring/config/controller/MainController.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.exception.StudentException;
 import com.spring.model.Student;
+import com.spring.model.StudentError;
 
 // http://localhost:8082/restful
 @RestController
@@ -55,13 +60,27 @@ public class MainController {
 	// http://localhost:8082/restful/main/getStudent/1
 	@GetMapping("/getStudent/{id}")
 	public Student getStudentById(@PathVariable("id") int id) {
+		if(id <= 0 || id > students.size()) {
+			throw new StudentException("Students not found .. id : "+id);
+		}
 		return students.get(id - 1);
 	}
 
 	// http://localhost:8082/restful/main/getStudentbyRequestParam?id=1
 	@GetMapping("/getStudentbyRequestParam")
 	public Student getStudentByIdRequestParam(@RequestParam int id) {
+		if(id <= 0 || id > students.size()) {
+			throw new StudentException("Students not found .. id : "+id);
+		}
 		return students.get(id - 1);
+	}
+	@ExceptionHandler
+	public ResponseEntity<StudentError> getException(StudentException se){
+		StudentError studentError = new StudentError();
+		studentError.setStatusCode(HttpStatus.NOT_FOUND.value());
+		studentError.setMessage(se.getMessage());
+		studentError.setTimeStamp(System.currentTimeMillis());
+		return new ResponseEntity<StudentError>(studentError, HttpStatus.NOT_FOUND);
 	}
 
 }
