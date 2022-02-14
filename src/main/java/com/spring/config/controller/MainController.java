e2d06e533a729dc25a671c4db2ddbd7a25842df6
mostafa <mosta.abdo250@gmail.com>
2022-02-14 19:30:29 +0200

    pathVariable

--- src/main/java/com/spring/config/controller/MainController.java
+++ src/main/java/com/spring/config/controller/MainController.java
@@ -4,7 +4,10 @@ package com.spring.config.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,20 @@ import com.spring.model.Student;
 // http://localhost:8082/restful/main
 public class MainController {
 	
+	private List<Student> students = new ArrayList<Student>();
+	
+	@PostConstruct
+	public void start() {
+		Student s1 = new Student(1, "mosta", "0101");
+		Student s2 = new Student(2, "momo", "0102");
+		Student s3 = new Student(3, "mostafafafa", "0103");
+		Student s4 = new Student(4, "mosalah", "0104");
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		students.add(s4);
+	}
+	
 	// http://localhost:8082/restful/main/mainpage
 	@GetMapping("/mainpage")
 	public String mainPage() {
@@ -24,15 +41,6 @@ public class MainController {
 	// http://localhost:8082/restful/main/students
 	@GetMapping("/students")
 	public List<Student> getStudents(){
-		Student s1 = new Student(1, "mosta", "0101");
-		Student s2 = new Student(2, "momo", "0102");
-		Student s3 = new Student(3, "mostafafafa", "0103");
-		Student s4 = new Student(4, "mosalah", "0104");
-		List<Student> students = new ArrayList<Student>();
-		students.add(s1);
-		students.add(s2);
-		students.add(s3);
-		students.add(s4);
 		return students;
 	}
 	
@@ -41,6 +49,12 @@ public class MainController {
 	public Student getStudent(){
 		Student s1 = new Student(100, "one students", "011");
 		return s1;
+	}
+	
+	// http://localhost:8082/restful/main/getStudent/1
+	@GetMapping("/getStudent/{id}")
+	public Student getStudentById(@PathVariable("id")int id){
+		return students.get(id - 1);
 	}
 	
 }
