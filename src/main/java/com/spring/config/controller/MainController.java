ffd0d6c5f62cb58ba4737eadc635c9657555644b
mostafa <mosta.abdo250@gmail.com>
2022-02-14 19:38:29 +0200

    request param

--- src/main/java/com/spring/config/controller/MainController.java
+++ src/main/java/com/spring/config/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.spring.config.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,17 +8,19 @@ import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.model.Student;
+
 // http://localhost:8082/restful
 @RestController
 @RequestMapping("/main")
 // http://localhost:8082/restful/main
 public class MainController {
-	
+
 	private List<Student> students = new ArrayList<Student>();
-	
+
 	@PostConstruct
 	public void start() {
 		Student s1 = new Student(1, "mosta", "0101");
@@ -31,30 +32,36 @@ public class MainController {
 		students.add(s3);
 		students.add(s4);
 	}
-	
+
 	// http://localhost:8082/restful/main/mainpage
 	@GetMapping("/mainpage")
 	public String mainPage() {
 		return "hello mosta";
 	}
-	
+
 	// http://localhost:8082/restful/main/students
 	@GetMapping("/students")
-	public List<Student> getStudents(){
+	public List<Student> getStudents() {
 		return students;
 	}
-	
+
 	// http://localhost:8082/restful/main/student
 	@GetMapping("/student")
-	public Student getStudent(){
+	public Student getStudent() {
 		Student s1 = new Student(100, "one students", "011");
 		return s1;
 	}
-	
+
 	// http://localhost:8082/restful/main/getStudent/1
 	@GetMapping("/getStudent/{id}")
-	public Student getStudentById(@PathVariable("id")int id){
+	public Student getStudentById(@PathVariable("id") int id) {
 		return students.get(id - 1);
 	}
-	
+
+	// http://localhost:8082/restful/main/getStudentbyRequestParam?id=1
+	@GetMapping("/getStudentbyRequestParam")
+	public Student getStudentByIdRequestParam(@RequestParam int id) {
+		return students.get(id - 1);
+	}
+
 }
